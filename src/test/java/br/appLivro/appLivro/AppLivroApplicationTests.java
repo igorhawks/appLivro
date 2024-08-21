86b4f7d8847ec89c464aca3b309f091b7783ca07
Igor Silva <igor719@gmail.com>
2024-08-21 19:01:40 -0300

    Iniciando o projeto

---
+++ src/test/java/br/appLivro/appLivro/AppLivroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.appLivro.appLivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppLivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
